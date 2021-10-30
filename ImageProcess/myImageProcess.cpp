05e3f1928a68c9c550ddbec255119c0766c98fe5
林轶锽（Lin Yihuang）(linyihuang) <linyihuang@qiyi.com>
2021-10-31 01:15:23 +0800

    first commit

---
+++ ImageProcess/myImageProcess.cpp
@@ -0,0 +1,5 @@
+#include "myImageProcess.h"
+
+double double getSum(const double num1, const double num2){
+    return num1 + num2;
+}
---
+++ ImageProcess/myImageProcess.h
@@ -0,0 +1,4 @@
+class myImageProcesss{
+    public:
+    double getSum(const double num1, const double num2);
+};
